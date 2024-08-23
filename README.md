ee249811dec0194ba7edb13c345a7f1a84a89386
Chris Lane Jones <chrislanejones@gmail.com>
2024-08-22 23:57:47 -0400

    Adding PerformanceMonitor

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Truck Animation
 
 Truck Gltf File - Renault Master L2H2 Panel Van 2020 - tedpermana (https://sketchfab.com/tedpermana)
+
+Added Van with Driver
--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,13 @@ import "./App.css";
 import Experience from "./components/Experience";
 import * as THREE from "three";
 import React, { useRef } from "react";
-import { PerspectiveCamera } from "@react-three/drei";
+import { PerformanceMonitor, PerspectiveCamera } from "@react-three/drei";
 import { Leva, useControls } from "leva";
 
 function App() {
   return (
     <Canvas camera={{ position: [16, 9, 12], fov: 70 }}>
+      <PerformanceMonitor />
       {/* <CameraControls /> */}
       <Experience />
     </Canvas>
