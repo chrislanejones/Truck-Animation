a40414accf0639f9f29bfa5f5b17bc447e454b7f
Chris Lane Jones <chrislanejones@gmail.com>
2024-08-26 23:55:51 -0400

    Initializing Theatre JS

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,23 @@ import "./App.css";
 import Experience from "./components/Experience";
 import React, { useRef, useState } from "react";
 import { Perf } from "r3f-perf";
+import { getProject } from "@theatre/core";
+import { SheetProvider } from "@theatre/r3f";
+
+const demoSheet = getProject("vanproject").sheet("van");
+
+import studio from "@theatre/studio";
+
+studio.initialize();
 
 function App() {
   return (
     <>
       <Canvas camera={{ position: [16, 9, 12], fov: 70 }}>
-        {/* <Perf /> */}
-        <Experience />
+        <SheetProvider sheet={van}>
+          {/* <Perf /> */}
+          <Experience />
+        </SheetProvider>
       </Canvas>
     </>
   );
