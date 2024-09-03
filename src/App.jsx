d03e722b08be9d7490bd1066a57889676abb66ae
Chris Lane Jones <chrislanejones@gmail.com>
2024-09-03 01:21:26 -0400

    Created and Fixed PerspectiveCamera for Theatre.JS

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-import { Canvas, useFrame } from "@react-three/fiber";
-import "./App.css";
-import Experience from "./components/Experience";
-import React, { useRef, useState } from "react";
+import { Canvas } from "@react-three/fiber";
+import React, { useRef } from "react";
 import { Perf } from "r3f-perf";
 import { getProject } from "@theatre/core";
-import { SheetProvider } from "@theatre/r3f";
-import { PerspectiveCamera } from "@theatre/r3f";
+import { PerspectiveCamera, SheetProvider, editable as e } from "@theatre/r3f";
+import "./App.css";
+import Experience from "./components/Experience";
 
 import studio from "@theatre/studio";
 import extension from "@theatre/r3f/dist/extension";
@@ -17,14 +16,33 @@ const project = getProject("VanProject");
 const mainSheet = project.sheet("Main");
 
 function App() {
+  const cameraTargetRef = useRef();
   return (
     <>
       <Canvas
-        camera={{ position: [10, 9, 12], fov: 60, near: 1 }}
+        camera={{ position: [0, 0, 0], fov: 80, near: 1 }}
         shadows
         gl={{ preserveDrawingBuffer: true }}
       >
         <SheetProvider sheet={mainSheet}>
+          <PerspectiveCamera
+            position={[14, 10, 35]}
+            fov={30}
+            near={1}
+            makeDefault
+            theatreKey="Camera"
+            lookAt={cameraTargetRef}
+          />
+          <e.mesh
+            position={[0, 6, 8]}
+            theatreKey="Camera Target"
+            visible="editor"
+            ref={cameraTargetRef}
+          >
+            <octahedronGeometry args={[0.1, 0]} />
+            <meshPhongMaterial color="yellow" />
+          </e.mesh>
+
           <Experience />
         </SheetProvider>
       </Canvas>
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -26,52 +26,54 @@ function Experience() {
     <>
       <OrbitControls />
       {/* <ambientLight intensity={Math.PI / 2} /> */}
-      <Environment
-        background={true}
-        preset={"park"}
-        backgroundRotation={[0, Math.PI / 3, 0]}
-        environmentIntensity={0}
-      />
-      {/* <spotLight
+      <e.group theatreKey="World">
+        <Environment
+          background={true}
+          preset={"park"}
+          backgroundRotation={[0, Math.PI / 3, 0]}
+          environmentIntensity={0}
+        />
+        {/* <spotLight
         position={[10, 10, 10]}
         angle={0.15}
         penumbra={1}
         decay={0}
         intensity={Math.PI}
       /> */}
-      {/* <Van scale={0.05} /> */}
-      <e.group theatreKey="Van">
-        <VanDoors scale={0.05} />
-        <Avatarclj
-          rotation-y={Math.PI}
-          rotation-x={Math.PI / 2}
-          rotation-z={Math.PI}
-          scale={4.5}
-          position={[-0.4, 3.7, 4.8]}
-        />
-      </e.group>
-      <mesh position={[0, -0.2, 0]} rotation-x={-Math.PI / 2}>
-        <boxGeometry args={[200, 210]} />
-        <meshStandardMaterial
-          map={texture}
-          color="darkgray"
-          emissive="black"
-          emissiveIntensity={0.9}
-        />
-        {/* <MeshReflectorMaterial
+        {/* <Van scale={0.05} /> */}
+        <e.group theatreKey="Van">
+          <VanDoors scale={0.05} />
+          <Avatarclj
+            rotation-y={Math.PI}
+            rotation-x={Math.PI / 2}
+            rotation-z={Math.PI}
+            scale={4.5}
+            position={[-0.4, 3.7, 4.8]}
+          />
+        </e.group>
+        <mesh position={[0, -0.2, 0]} rotation-x={-Math.PI / 2}>
+          <boxGeometry args={[200, 210]} />
+          <meshStandardMaterial
+            map={texture}
+            color="darkgray"
+            emissive="black"
+            emissiveIntensity={0.9}
+          />
+          {/* <MeshReflectorMaterial
           color="#414141"
           resolution={1024}
           mixStrength={3}
           roughness={0.6}
         /> */}
-      </mesh>
-      <e.pointLight
-        theatreKey="SunLight"
-        position={[100, 20, -10]}
-        decay={0}
-        color="#FFC300"
-        intensity={Math.PI * 0.4}
-      />
+        </mesh>
+        <e.pointLight
+          theatreKey="SunLight"
+          position={[100, 20, -10]}
+          decay={0}
+          color="#FFC300"
+          intensity={Math.PI * 0.4}
+        />
+      </e.group>
     </>
   );
 }
