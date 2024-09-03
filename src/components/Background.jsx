3ec9b05014643f5e02548cd7a734c6a186674687
Chris Lane Jones <chrislanejones@gmail.com>
2024-09-03 01:55:02 -0400

    Cleaned up Files and Added json for Theatre.JS

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -2,12 +2,27 @@ import { useTexture } from "@react-three/drei";
 import * as THREE from "three";
 
 export const Background = () => {
+  const texture = useTexture("textures/Road-Asphalt.jpg");
+
+  texture.repeat.set(10, 10);
+  texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
   const map = useTexture("textures/Euro-Urban-Street.jpg");
   return (
     <>
-      <mesh>
-        <sphereGeometry args={[400, 400, 150]} />
-        <meshBasicMaterial side={THREE.BackSide} map={map} toneMapped={false} />
+      <mesh position={[0, -0.2, 0]} rotation-x={-Math.PI / 2}>
+        <boxGeometry args={[200, 210]} />
+        <meshStandardMaterial
+          map={texture}
+          color="darkgray"
+          emissive="black"
+          emissiveIntensity={0.9}
+        />
+        {/* <MeshReflectorMaterial
+          color="#414141"
+          resolution={1024}
+          mixStrength={3}
+          roughness={0.6}
+        /> */}
       </mesh>
     </>
   );
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,30 +1,13 @@
-import {
-  OrbitControls,
-  MeshReflectorMaterial,
-  useGLTF,
-  PerspectiveCamera,
-  useTexture,
-  Environment,
-} from "@react-three/drei";
+import { useTexture, Environment } from "@react-three/drei";
 import * as THREE from "three";
-import { Van } from "./Van";
-import { useRef } from "react";
-import { useFrame } from "@react-three/fiber";
 import { Avatarclj } from "./Avatarclj";
 import { VanDoors } from "./Van-With-Rear-Door";
 import { Background } from "./Background";
 import { editable as e } from "@theatre/r3f";
-import { getProject, types } from "@theatre/core";
 
 function Experience() {
-  const texture = useTexture("textures/Road-Asphalt.jpg");
-
-  texture.repeat.set(10, 10);
-  texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
-
   return (
     <>
-      <OrbitControls />
       {/* <ambientLight intensity={Math.PI / 2} /> */}
       <e.group theatreKey="World">
         <Environment
@@ -51,21 +34,7 @@ function Experience() {
             position={[-0.4, 3.7, 4.8]}
           />
         </e.group>
-        <mesh position={[0, -0.2, 0]} rotation-x={-Math.PI / 2}>
-          <boxGeometry args={[200, 210]} />
-          <meshStandardMaterial
-            map={texture}
-            color="darkgray"
-            emissive="black"
-            emissiveIntensity={0.9}
-          />
-          {/* <MeshReflectorMaterial
-          color="#414141"
-          resolution={1024}
-          mixStrength={3}
-          roughness={0.6}
-        /> */}
-        </mesh>
+        <Background />
         <e.pointLight
           theatreKey="SunLight"
           position={[100, 20, -10]}
