998582cee5c80c00956da70d467c97f1ebeec153
Chris Lane Jones <chrislanejones@gmail.com>
2024-09-05 23:51:50 -0400

    Adding Threatre Key Controls to Rear Doors

--- src/components/Branding.jsx
+++ src/components/Branding.jsx
@@ -1,24 +1,23 @@
 import { Decal } from "@react-three/drei";
 import React from "react";
+import { editable as e } from "@theatre/r3f";
 
 const Branding = () => {
   return (
     <>
-      <mesh>
+      <e.mesh theatreKey="decal">
         <sphereGeometry />
         <meshBasicMaterial />
         <Decal
           debug // Makes "bounding box" of the decal visible
-          position={[0, 0, 0]} // Position of the decal
-          rotation={[0, 0, 0]} // Rotation of the decal (can be a vector or a degree in radians)
-          scale={1} // Scale of the decal
+          position={[0, 0.9, 0.75]} rotation={[-0.4, Math.PI, 0]} scale={[0.9, 0.25, 1]}
         >
           <meshBasicMaterial
             polygonOffset
             polygonOffsetFactor={-1} // The material should take precedence over the original
           />
         </Decal>
-      </mesh>
+      </e.mesh>
     </>
   );
 };
